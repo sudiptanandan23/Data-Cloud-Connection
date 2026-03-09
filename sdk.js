e34604f09e7de30dd58ce07b6d8f758545eda263
sudiptanandan23 <sudiptanandan23@gmail.com>
2026-03-09 21:34:24 +0530

    Update sdk.js

--- sdk.js
+++ sdk.js
@@ -1,100 +1,31 @@
-
-
 <script>
-/* Salesforce Data Cloud Sitemap Configuration */
-
-document.addEventListener("DOMContentLoaded", function () {
-
 const sitemapConfig = {
+  global: {
+    onActionEvent: function (event) {
 
-global: {
-onActionEvent: function (event) {
-console.log("Data Cloud Event Sent:", event);
-return event;
-}
-},
+      event.pageUrl = window.location.href;
+      event.pageName = document.title;
+      event.referrer = document.referrer;
+      event.browser = navigator.userAgent;
 
-pageTypes: [
+      return event;
+    }
+  },
 
-{
-name: "home_page",
+  pageTypes: [
 
-isMatch: function () {
-return window.location.pathname.includes("index") ||
-window.location.pathname === "/" ||
-window.location.pathname.includes("Data-Cloud-Connection");
-},
+    {
+      name: "Home Page",
+      interaction: {
+        name: "ViewPage"
+      },
+      isMatch: () =>
+        window.location.pathname === "/" ||
+        window.location.pathname.includes("Data-Cloud-Connection")
+    }
 
-interaction: {
-name: "Home Page View"
-}
-
-},
-
-{
-name: "about_page",
-
-isMatch: function () {
-return window.location.pathname.includes("about");
-},
-
-interaction: {
-name: "About Page View"
-}
-
-},
-
-{
-name: "contact_page",
-
-isMatch: function () {
-return window.location.pathname.includes("contact");
-},
-
-interaction: {
-name: "Contact Page View"
-}
-
-}
-
-]
-
+  ]
 };
 
-
-/* Initialize Salesforce Data Cloud */
-
-if (window.SalesforceInteractions) {
-
-SalesforceInteractions.init({
-
-cookieDomain: window.location.hostname,
-sitemapConfig: sitemapConfig
-
-});
-
-}
-
-
-/* Send Test Event */
-
-setTimeout(function(){
-
-if (window.SalesforceInteractions) {
-
-SalesforceInteractions.sendEvent({
-
-interaction: {
-name: "Website Visit"
-}
-
-});
-
-}
-
-},2000);
-
-});
-
-
+SalesforceInteractions.initSitemap(sitemapConfig);
 </script>
